e9839c8e15856c3c7d31521341995927825e2e07
Patrick Shepherd <patrick.shepherd@ans.co.uk>
2023-07-19 09:13:54 +0100

    Add feature flags for all module resources

--- aws/codedeploy_workflow/build/main.tf
+++ aws/codedeploy_workflow/build/main.tf
@@ -3,11 +3,13 @@ locals {
 }
 
 resource "aws_iam_role" "codepipeline_role" {
+  count = var.enable_codepipeline_role == true ? 1 : 0
   name               = "${local.pipeline_name}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 module "kms_key" {
+  count = var.enable_kms_key == true ? 1 : 0
   source           = "../../kms_key"
   key_name         = "${local.pipeline_name}-kms-key"
   dest_account_ids = data.aws_arn.deployment_role.*.account
@@ -17,6 +19,7 @@ module "kms_key" {
 }
 
 module "pipeline_bucket" {
+  count = var.enable_pipeline_bucket == true ? 1 : 0
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "${local.pipeline_name}-pipeline-bucket"
@@ -42,6 +45,7 @@ module "pipeline_bucket" {
 }
 
 module "deploy_bucket" {
+  count = var.enable_deploy_bucket == true ? 1 : 0
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "${local.pipeline_name}-deploy-bucket"
@@ -67,6 +71,7 @@ module "deploy_bucket" {
 }
 
 resource "aws_codepipeline" "codepipeline" {
+  count = var.enable_codepipeline == true ? 1 : 0
   for_each = toset(var.branches)
 
   name     = "${local.pipeline_name}-${each.value}-pipeline"
--- aws/codedeploy_workflow/build/variables.tf
+++ aws/codedeploy_workflow/build/variables.tf
@@ -17,3 +17,33 @@ variable "code_commit_repo" {
   description = "Name of the Code Commit Repo"
   type        = string
 }
+
+variable "enable_codepipeline_role" {
+  description = "Whether to include the CodePipeline role"
+  type = bool
+  default = true
+}
+
+variable "enable_kms_key" {
+  description = "Whether to include the KMS key"
+  type = bool
+  default = true
+}
+
+variable "enable_pipeline_bucket" {
+  description = "Whether to include the pipeline bucket"
+  type = bool
+  default = true
+}
+
+variable "enable_deploy_bucket" {
+  description = "Whether to include the deploy bucket"
+  type = bool
+  default = true
+}
+
+variable "enable_codepipeline" {
+  description = "Whether to include the CodePipeline"
+  type = bool
+  default = true
+}
