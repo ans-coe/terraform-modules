45de60003b03c6b35b16ea844267f73660ab9fb7
Jon Kelly <jon.kelly@ans.co.uk>
2023-09-06 11:15:30 +0100

    changing variable default from empty to null

--- azure/firewall/variables.tf
+++ azure/firewall/variables.tf
@@ -71,13 +71,13 @@ variable "firewall_sku_tier" {
 variable "firewall_dns_servers" {
   description = "List of DNS Servers for Firewall config"
   type        = list(string)
-  default     = []
+  default     = null
 }
 
 variable "private_ip_ranges" {
   description = "A list of SNAT private CIDR IP ranges, or the special string IANAPrivateRanges, which indicates Azure Firewall does not SNAT when the destination IP address is a private range per IANA RFC 1918."
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = null
 }
 
 variable "firewall_policy_id" {
@@ -88,17 +88,6 @@ variable "firewall_policy_id" {
 
 variable "firewall_zones" {
   description = "Specifies a list of Availability Zones in which this Azure Firewall should be located."
-  type = list(string)
-  default = null
-}
-
-variable "ip_configuration" {
-  description = "Specifies public and private IP information of exsisting resources"
-  type = optional(object({
-    name = optional(string)
-    subnet_id = optional(string)
-    public_ip_address_id = optional(string)
-    private_ip_address_id = optional(string)
-  }))
-  default = null
+  type        = list(string)
+  default     = null
 }
