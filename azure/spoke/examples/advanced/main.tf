dcee96dd70143400aa2f4f05706cc2e2c4e1781f
Jon Kelly <jon.kelly@ans.co.uk>
2024-01-12 15:23:16 +0000

    adding rd deletion feature to examples

--- azure/spoke/examples/advanced/main.tf
+++ azure/spoke/examples/advanced/main.tf
@@ -4,13 +4,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85"
+      version = "~> 3.86"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 locals {
