7c3cd4ea77075f03ccd91a3b3c8d52dc6e943f9f
Jon Kelly <jon.kelly@ans.co.uk>
2024-01-15 14:10:34 +0000

    remove nw resource group

--- azure/spoke/main.tf
+++ azure/spoke/main.tf
@@ -80,22 +80,14 @@ module "route-table" {
 # Network Watcher
 ##################
 
-# Conditions for resource group:
-# If network_watcher_resource_group is specified = create network watcher RG
-# If network_watcher_resource_group is unspecified = spoke resource group
-
-resource "azurerm_resource_group" "network_watcher" {
-  count = var.network_watcher_resource_group != null ? 1 : 0
-
-  name     = var.network_watcher_resource_group
-  location = var.location
-  tags     = var.tags
-}
+# Conditions for Network Watcher variables:
+# If var.network_watcher_name is not specificed, use "network-watcher-${var.location}"
+# If var.network_watcher_resource_group_name is not specified, use var.resource_group_name
 
 resource "azurerm_network_watcher" "main" {
   count = var.create_network_watcher ? 1 : 0
 
-  name                = var.network_watcher_name != null ? var.network_watcher_name : "nw-${var.location}"
+  name                = local.network_watcher_name
   location            = var.location
   resource_group_name = local.network_watcher_resource_group_name
   tags                = var.tags
