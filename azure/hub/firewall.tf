a17d5f0a368df05c7fdc8a093996ab51eacacbf3
Jon Kelly <jon.kelly@ans.co.uk>
2024-06-03 15:44:01 +0100

    fixing fw rt naming

--- azure/hub/firewall.tf
+++ azure/hub/firewall.tf
@@ -15,7 +15,7 @@ module "firewall" {
   pip_name                = var.firewall["public_ip_name"] == null ? format("pip-%s", var.firewall["name"]) : var.firewall["public_ip_name"]
   subnet_address_prefixes = [var.firewall["address_prefix"]]
 
-  route_table_name = var.route_table_name != null ? var.route_table_name : "rt-azfw-${module.network.name}"
+  route_table_name = var.firewall["route_table_name"] != null ? var.firewall["route_table_name"] : "rt-azfw-${module.network.name}"
 
   firewall_sku_name  = "AZFW_VNet"
   firewall_sku_tier  = var.firewall["sku_tier"]
